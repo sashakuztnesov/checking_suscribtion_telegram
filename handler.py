980fcfbefa607132a9352ac541bba4fee0a6b3f0
sashakuztnesov <110970672+sashakuztnesov@users.noreply.github.com>
2022-08-10 11:34:27 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,52 +1,55 @@
-import json
+import telebot
+import sqlite3 as sql
 
-def handle(data):
 
-    data = json.loads(data)
-    user_answer = data["user_answer"]
-    value_01 = data["value_01"]
-    if data["value_02"]:
+#База Данных
+con = sql.connect("data.db", check_same_thread=False)
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS users(id INT)")
 
-        if user_answer and value_01:
-            user_answer = int(data["user_answer"])
-            value_01 = int(data["value_01"])
-            value_02 = int(data["value_02"])
-            strg = data["strg"]
+#Ваш бот
+token = ""
+bot = telebot.TeleBot(token)
 
-            if strg == "s":
-                if user_answer < value_01 or user_answer==value_01:
-                   status = -1
-                if value_01 < user_answer < value_02:
-                    status = 0
-                if user_answer > value_02 or user_answer==value_02:
-                    status = 1
+#ID Вашего канала
+chan_id = https://t.me/GalaRazhisByb
 
-            else:
+#Клавиатура для проверки подписки
+keyboard = telebot.types.InlineKeyboardMarkup()
+subscribe = telebot.types.InlineKeyboardButton(text="Подписаться", url="ссылка на ваш канал")
+check = telebot.types.InlineKeyboardButton(text="Проверить", callback_data="check")
+keyboard.add(subscribe)
+keyboard.add(check)
 
-                if user_answer <= value_01:
-                    status = -1
-                if value_01 <= user_answer <= value_02:
-                    status = 0
-                if user_answer > value_02:
-                    status = 1
+#Не нужная хуйня, вписал просто для видимости
+menu = telebot.types.ReplyKeyboardMarkup(True, True)
+menu.add("Вы уже подписаны")
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    users = cur.execute("SELECT id FROM users WHERE id = ?", (message.chat.id, )).fetchone()
+    con.commit()
+    if users == None: #Если юзер ещё не в БД
+        bot.send_message(message.chat.id, "Что-бы продолжить подпишитесь на канал", reply_markup=keyboard)
+    else: #Уже в бд
+        bot.send_message(message.chat.id, "Вы уже подписаны", reply_markup=menu)
+
+
+#Тут мы чекаем подписку
+@bot.callback_query_handler(func=lambda call: True)
+def c_listener(call):
+    if call.data == "check":
+        x = bot.get_chat_member(chan_id, call.message.chat.id)
+
+        if x.status == "member" or x.status == "creator" or x.status == "administrator":
+            bot.send_message(call.message.chat.id, "Добро пожаловать!", reply_markup=menu)
+            cur.execute("INSERT INTO users VALUES(?)", (call.message.chat.id, ))
+            con.commit()
         else:
-            status = 2
-        status = json.dumps({"status": status})
-    else:
-        if user_answer and value_01:
-            user_answer = int(data["user_answer"])
-            value_01 = int(data["value_01"])
+            bot.send_message(call.message.chat.id, "Вы не подписались!", reply_markup=keyboard)
 
-            if user_answer < value_01:
-                status = -1
-            if user_answer > value_01:
-                status = 1
-            if user_answer == value_01:
-                status = 0
-        else:
-            status = 2
 
-        status = json.dumps({"status": status})
 
-    return status
-
+if __name__ == "__main__":
+    bot.polling()
